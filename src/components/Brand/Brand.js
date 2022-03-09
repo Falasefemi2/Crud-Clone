f3c65d2c0dbb4381cc34590f32203d1bd7172821
Femi <femifalase228@gmail.com>
2022-03-09 17:37:23 +0100

    deploy cred clone

---
+++ src/components/Brand/Brand.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./brand.css";
+
+const Brand = () => {
+  return (
+    <div className='brand'>
+        <div className="max-width">
+            <div className='brand-heading'>rewards from brands you love.</div>
+            <div className="brand-banner"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Brand
---
+++ src/components/WindowPeak/WindowPeak.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./windowpeak.css";
+
+const WindowPeak = () => {
+  return (
+    <div className='window-peak'></div>
+  )
+}
+
+export default WindowPeak
